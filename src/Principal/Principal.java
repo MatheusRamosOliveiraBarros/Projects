ff39dc918b0719c8a2c9a1579734d699f2397202
Alexandre <alexandre.andriani.jr@gmail.com>
2024-05-07 16:00:22 -0300

    parte gráfica feita

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,10 +1,18 @@
 package Principal;
 
+import javax.swing.JFrame;
+
+import view.MainFrame;
 
 public class Principal {
 
     public static void main(String[] args) {
-    
+        MainFrame tela = new MainFrame();
+        tela.iniciar();
+        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // comportamento ao fechar a janela
+        tela.setSize(975, 550);
+        tela.setLocationRelativeTo(null); // Centralizar a janela
+        tela.setVisible(true);
     }
 
 }
--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,15 +1,120 @@
 package view;
 
 import javax.swing.JFrame;
-import javax.swing.WindowConstants;
-import javax.swing.plaf.DimensionUIResource;
+
+
 
 public class MainFrame extends JFrame {
-    public void iniciar() {
-        setTitle("Welcome");
-        setSize(500, 600);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
 
+    
+    public void iniciar() {
+
+        jFrame1 = new javax.swing.JFrame();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
+    
+        javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
+        jFrame1.getContentPane().setLayout(jFrame1Layout);
+        jFrame1Layout.setHorizontalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jFrame1Layout.setVerticalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+    
+        setLayout(new java.awt.BorderLayout());
+    
+        jPanel1.setBackground(new java.awt.Color(22, 22, 30));
+        jPanel1.setPreferredSize(new java.awt.Dimension(250, 477));
+        jPanel1.setRequestFocusEnabled(false);
+    
+        jLabel1.setFont(new java.awt.Font("DejaVu Sans", 0, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(192, 202, 245));
+        jLabel1.setText("Seja Bem-vindo!");
+    
+        jButton1.setBackground(new java.awt.Color(22, 22, 30));
+        jButton1.setForeground(new java.awt.Color(242, 242, 242));
+        jButton1.setText("Adicionar Produto");
+    
+        jButton2.setBackground(new java.awt.Color(22, 22, 30));
+        jButton2.setForeground(new java.awt.Color(242, 242, 242));
+        jButton2.setText("Editar produto");
+    
+        jButton3.setBackground(new java.awt.Color(22, 22, 30));
+        jButton3.setForeground(new java.awt.Color(242, 242, 242));
+        jButton3.setText("Listar produtos");
+    
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(53, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jButton3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addGap(51, 51, 51))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addGap(18, 18, 18)
+                .addComponent(jButton3)
+                .addContainerGap(296, Short.MAX_VALUE))
+        );
+    
+        add(jPanel1, java.awt.BorderLayout.WEST);
+    
+        jPanel2.setMinimumSize(new java.awt.Dimension(300, 300));
+        jPanel2.setName(""); // NOI18N
+        jPanel2.setPreferredSize(new java.awt.Dimension(650, 477));
+        jPanel2.setRequestFocusEnabled(false);
+        jPanel2.setLayout(new java.awt.BorderLayout());
+    
+        jList1.setBackground(new java.awt.Color(26, 27, 38));
+        jList1.setFont(new java.awt.Font("Noto Mono", 0, 14)); // NOI18N
+        jList1.setForeground(new java.awt.Color(242, 242, 242));
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(jList1);
+    
+        jPanel2.add(jScrollPane1, java.awt.BorderLayout.CENTER);
+    
+        add(jPanel2, java.awt.BorderLayout.EAST);
+    }// </editor-fold>                        
+    
+    
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JFrame jFrame1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration
 }
+
+
