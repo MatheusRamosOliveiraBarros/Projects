03a0287af46087c82f59a80cb74d56e08de27e73
BinattiLeonardo <tobybinatti@hotmail.com>
2024-05-15 16:02:21 -0300

    Model comida.java pronto

--- src/Model/Comida.java
+++ src/Model/Comida.java
@@ -3,10 +3,29 @@ package Model;
 import java.util.Date;
 
 public class Comida extends Produto {
+    private int tipoDeProduto = 1; 
 
     public Comida(int id, String nome, String descricao, int quantidadeEstoque, float preco, Date dataCadastro) {
         super(id, nome, descricao, quantidadeEstoque, preco, dataCadastro);
-        //TODO Auto-generated constructor stub
+        setTipoDeProduto(tipoDeProduto);
     }
     
+        public Comida(String nome, int quantidadeEstoque, float preco) {
+        super(nome, quantidadeEstoque, preco);
+        setTipoDeProduto(tipoDeProduto);
+        }
+
+    public Comida() {
+     super(0, "", "", 0, 0.0f, new Date());
+    }
+
+
+    public int getTipoDeProduto() {
+        return tipoDeProduto;
+    }
+    public void setTipoDeProduto(int tipoDeProduto) {
+        this.tipoDeProduto = tipoDeProduto;
+    }
+
+
 }
