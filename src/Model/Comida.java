eea848cf12b07bf58f9b39245808a6224a2b55c4
Alexandre <alexandre.andriani.jr@gmail.com>
2024-05-24 03:18:28 -0300

    login feito

--- src/Model/Comida.java
+++ src/Model/Comida.java
@@ -5,7 +5,7 @@ import DAO.ProdutoDAO;
 import java.util.ArrayList;
 
 public class Comida extends Produto {
-    // Número 1 para bebidas
+    // Número 1 para comidas
     private int tipoDeProduto = 1;
     private ProdutoDAO dao; 
 
@@ -19,11 +19,39 @@ public class Comida extends Produto {
         this.dao = new ProdutoDAO();
     }
 
+    public Comida(String nome, String descricao, int quantidadeEstoque, float valor) {
+        super(0, nome, descricao, quantidadeEstoque, valor, new Date());
+        this.dao = new ProdutoDAO();
+    }
+
+    public Comida(int id, String nome, String descricao, int quantidadeEstoque, float valor) {
+        super(id ,nome, descricao, quantidadeEstoque, valor, new Date());
+        this.dao = new ProdutoDAO();
+    }
+
     public int getTipoDeProduto() {
         return tipoDeProduto;
     }
 
     public ArrayList getLista() {
         return dao.getLista(this.tipoDeProduto);
+    }
+    public boolean Add_comida(String nome, String descricao, float valor, int quantidadeEstoque) {
+        Comida objeto = new Comida(nome, descricao, quantidadeEstoque, valor);
+        ProdutoDAO.Lista.add(objeto);
+        dao.InsertProduto(objeto, tipoDeProduto);
+        return true;
+    }
+
+    public boolean Editar_comida(int id, String nome, String descricao, float valor, int quantidadeEstoque) {
+        Comida objeto = new Comida(id, nome, descricao, quantidadeEstoque, valor);
+        ProdutoDAO.Lista.add(objeto);
+        dao.UpdateProduto(objeto);
+        return true;
+    }
+
+    public boolean DeleteProduto(int idComida) {
+        dao.DeleteProduto(idComida);
+        return true;
     }
 }
